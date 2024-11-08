c85ad5d55c74e7c142b5753dbe23c49a05f3bafb
Vladimir Le1to <ikigainspire@gmail.com>
2024-11-08 21:48:08 +0300

    feat: created navbar w/ navbar points

--- frontend/src/components/header/ui/header.tsx
+++ frontend/src/components/header/ui/header.tsx
@@ -1,3 +1,19 @@
+import { HeaderItems } from "../model/header-items";
+
 export const Header = () => {
-  return <header>header</header>;
+  return (
+    <header className="bg-white shadow-md p-4 w-full">
+      <nav className="container mx-auto flex justify-center space-x-8">
+        {HeaderItems.map((item) => (
+          <a
+            key={item.href}
+            href={item.href}
+            className="text-pale-black hover:text-main-blue transition-colors duration-300"
+          >
+            {item.value}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
 };
