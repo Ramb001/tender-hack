300daa24c35456ee28307182a338ad8e01c3d93f
SenezAr <danila.lusuy@gmail.com>
2024-11-08 22:47:21 +0300

    fix: refactoring

--- frontend/src/components/upload-form/model/upload-form-schema.ts
+++ frontend/src/components/upload-form/model/upload-form-schema.ts
@@ -1,16 +1,5 @@
 import { z } from "zod";
-
-// Схема для одного URL
-export const formSchemaSingle = z.object({
-  url: z
-    .string()
-    .url("Введите корректный URL") // Проверка на корректный URL
-    .nonempty("Поле не может быть пустым"), // Поле обязательно
-});
-
-// Схема для списка URL
-export const formSchemaList = z.object({
-  urls: z
-    .array(z.string().url("Некорректный URL")) // Проверка на массив URL
-    .nonempty("Введите хотя бы одну ссылку"), // Поле обязательно, массив не может быть пустым
+export const formSchema = z.object({
+  file: z.instanceof(File),
+  url: z.string().url(),
 });
